85a7ffd5080f343b28f1be4fec5b20fa4034830a
guemboy <manussawin@gmail.com>
2017-09-09 00:58:01 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -2,7 +2,7 @@
 
 // example: https://github.com/onlinetuts/line-bot-api/blob/master/php/example/chapter-01.php
 
-include ('line-bot-api/php/line-bot.php');
+//include ('line-bot-api/php/line-bot.php');
 
 $channelSecret = 'e797689c9f1df5a32be4239b79dda174';
 $access_token  = '94BkkcvLjVdd4fgFBpI8+IXfz2yCJbI4lij8EZojyCl+R92y6ZxqrEAbTtC8Ld9tFVhCAi6fa7YVYWJMSvWRqFaB/xNY2ziWMCk04yWXeLlH3IkNxeqMVe578CuZXmafInpy/+iQcXJCXv2glcvnugdB04t89/1O/w1cDnyilFU=';
